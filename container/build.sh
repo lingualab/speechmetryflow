0c1463d1191c184f6f4d78ba88294fd06d336120
Christophe Bedetti <christophe.bedetti@umontreal.ca>
2024-07-04 16:37:31 -0400

    Add praat scripts to container

--- container/build.sh
+++ container/build.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+#
+# Setup
+# apptainer key newpair
+# apptainer registry login --username lingualab docker://docker.io
 
 # Building
-container_name="speechmetryflow_0.0.1.sif"
+container_name="speechmetryflow_0.0.2.sif"
 apptainer build -F ${container_name} container.def
+
+# Signing
+# apptainer sign ${container_name}
+# apptainer push ${container_name} docker://lingualab/speechmetryflow:dev
 
 # Publishing
 rsync -av ${container_name} elm:/data/brambati/local/containers/lingualab/
