76a847aed3945a666beee79dfbbbf356cbd99291
Christophe Bedetti <christophe.bedetti@umontreal.ca>
2024-07-08 12:56:37 -0400

    Update build script for elm

--- container/build.sh
+++ container/build.sh
@@ -6,11 +6,10 @@
 
 # Building
 container_name="speechmetryflow_0.0.2.sif"
-apptainer build -F ${container_name} container.def
+apptainer build --fakeroot -F /data/brambati/local/containers/lingualab/${container_name} container.def
 
 # Signing
 # apptainer sign ${container_name}
 # apptainer push ${container_name} docker://lingualab/speechmetryflow:dev
 
 # Publishing
-rsync -av ${container_name} elm:/data/brambati/local/containers/lingualab/
